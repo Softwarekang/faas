c6c1e7b1d1c4c65782567b44c6a7283b2d0e3e66
ankang2 <ankang2@shein.com>
2025-04-24 16:10:37 +0800

    feat(faas): update replicas

--- gateway/scaling/ranges.go
+++ gateway/scaling/ranges.go
@@ -11,10 +11,10 @@ import (
 
 const (
 	// DefaultMinReplicas is the minimal amount of replicas for a service.
-	DefaultMinReplicas = 1
+	DefaultMinReplicas = 8
 
 	// DefaultMaxReplicas is the amount of replicas a service will auto-scale up to.
-	DefaultMaxReplicas = 5
+	DefaultMaxReplicas = 10
 
 	// DefaultScalingFactor is the defining proportion for the scaling increments.
 	DefaultScalingFactor = 10
